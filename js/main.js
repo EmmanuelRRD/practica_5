9f0f7ac4826aa920767d2053b537484905696bf2
EmmanuelRRD <159814753+EmmanuelRRD@users.noreply.github.com>
2024-09-29 16:04:09 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,121 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import Stats from 'three/addons/libs/stats.module.js';
+//import GUI from 'three/addons/libs/lil-gui.module.min.js';
+
+window.addEventListener('resize', onResize, false);
+
+const stats = Stats();
+document.body.appendChild( stats.dom );
+
+// Configuración básica del escenario
+const scene = new THREE.Scene();
+
+// configuración de la Camara Perspectiva
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set( 0,40,20 );
+camera.lookAt(scene.position);
+
+// Configuraciones para el renderizado del escenario
+const renderer = new THREE.WebGLRenderer( {antialias:true} );
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.VSMShadowMap;
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0x000000);
+document.body.appendChild( renderer.domElement );
+
+const orbitControl = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+
+// Crear y agregar un plano
+const planeGeometry = new THREE.PlaneGeometry(10000,10000);
+const planeMaterial = new THREE.MeshStandardMaterial({color:0xffffff});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.receiveShadow = true;
+plane.rotation.x = -0.5 * Math.PI;
+scene.add(plane);
+
+var textureLoader = new THREE.TextureLoader();
+
+const polyhendronGeometry = new THREE.IcosahedronGeometry(8,0);
+const polyhendronMaterial = new THREE.MeshStandardMaterial({
+    map: textureLoader.load('./images/2.jpg'),
+    metalness: 0.2,
+    roughness: 0.07
+});
+const polyhedron = new THREE.Mesh(polyhendronGeometry,polyhendronMaterial);
+polyhedron.castShadow = true;
+polyhedron.position.x = 20;
+polyhedron.position.y = 10;
+//polyhedron.scale.y = 2; 
+scene.add(polyhedron);
+
+//Circulito
+const spheraGeometry = new THREE.SphereGeometry(5,20,20);
+const spheraMaterial = new THREE.MeshStandardMaterial({
+    map: textureLoader.load('./images/1.jpg'),
+    metalness: 0.2,
+    roughness: 0.07
+});
+const sphere = new THREE.Mesh(spheraGeometry,spheraMaterial);
+sphere.position.y = 10;
+sphere.castShadow = true;
+scene.add(sphere);
+
+//Cubo
+const cubeGeometry = new THREE.BoxGeometry(10,10,10);
+const cubeMaterial = new THREE.MeshStandardMaterial({
+    map: textureLoader.load('./images/3.jpg'),
+    metalness: 0.2,
+    roughness: 0.07
+});
+const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
+cube.castShadow = true;
+cube.position.y = 10;
+cube.position.x = -20;
+scene.add(cube);
+
+const ambientLight = new THREE.AmbientLight(0xaaaaaa);
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight(0xaaaaaa);
+dirLight.position.set(20,40,40);
+dirLight.castShadow = true;
+dirLight.intensity = 2;
+dirLight.shadow.camera.near = 0.1;
+dirLight.shadow.camera.far = 200;
+dirLight.shadow.camera.right = 40;
+dirLight.shadow.camera.left = -40;
+dirLight.shadow.camera.top = 40;
+dirLight.shadow.camera.bottom = -40;
+dirLight.shadow.camera.width = 1024;
+dirLight.shadow.camera.height = 1024;
+dirLight.shadow.camera.radius = 4;
+dirLight.shadow.camera.bias = -0.0005;
+
+scene.add(dirLight);
+
+animate();
+
+
+function animate() {
+    requestAnimationFrame( animate );
+    stats.update();
+    orbitControl.update();
+
+
+    sphere.rotateY(0.01);
+    polyhedron.rotateY(0.01);
+
+
+    renderer.render( scene, camera );
+}
+
+function onResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
